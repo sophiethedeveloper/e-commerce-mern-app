155a4746f12b85cf74135d360b1d04a036295a4f
sofia Luna <Sofia21luna@gmail.com>
2020-11-06 19:02:03 -0600

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API IS RUNNING!");
@@ -16,4 +20,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("server running on port 5000"));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
