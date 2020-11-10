648fd7c90e2454352cc3b55f790fcd6a813eecd5
sofia Luna <Sofia21luna@gmail.com>
2020-11-09 20:07:46 -0600

    added product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const AsyncHandler = require("express-async-handler");
+const Product = require("../models/ProductModel");
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+// @description: fetch all products
+// @route GET /api/products
+// @access Public
+router.get(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+// @description: fetch single product
+// @route GET /api/products/:id
+// @access Public
+
+router.get(
+  "/:id",
+  AsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
 
 module.exports = router;
